79be5e4bb51712414741f81df7a66de4c7c006f7
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-01-21 14:44:30 +0100

    nye token ting, mangler sessionStorage

--- src/js/fetch-categories.js
+++ src/js/fetch-categories.js
@@ -1,28 +1,22 @@
-//no undercategories
-
-fetch("https://api.spotify.com/v1/recommendations/available-genre-seeds", {
-    method: "GET",
+fetch("https://api.spotify.com/v1/browse/categories", { 
     headers: {
-        Authorization: `Bearer BQAI3NIQ2Scz7bfQleKpzSf_4sGh1O6fQvxDS-86YT9k78DW2b0Pe6dshzA4roL8YzisPJy16Z23sX9CqJItu12bLGnvuiOslnUrp5RxREvIQa9yofeUT--DIFlHg0xR4x9Gi5TnBoCu2Zp2qDOnIw`     
+        "Accept" : "application/json",
+        "Content-Type" : "application/x-www-form-urlencoded",
+        "Authorization": "Bearer BQABT3PQ5zqqUASD9yCBVj_cjRxj4kDI1vfu8jyvneoDmt3gPbfTJNIz0OxRB6Y7axUmx-OcUHVwLIVh2Jw"
     }
     })
     
     .then(response => response.json())
-    .then(function(result) {
-        result.genres.forEach(item =>{
-            console.log(result);     
-
+    .then(function(result) {  
+        result.categories.items.forEach(item =>{
             document.querySelector(".categories").innerHTML += `
                 <div class="category">
-                    <h4 class="show-ul">${item} <i class="fas fa-ellipsis-h"></i></h4>
+                    <h4 class="show-ul">${item.name} <i class="fas fa-ellipsis-h"></i></h4>
                     <ul class="category-ul">
-                        <a href="albums.html?name=${item}><li>first<i class="fas fa-chevron-right"></i></li></a>
-                        <a href="albums.html?name=${item}"><li>second<i class="fas fa-chevron-right"></i></li></a>
-                        <a href="albums.html?name=${item}"><li>third<i class="fas fa-chevron-right"></i></li></a>
+                        <a href="albums.html?name=${item.name}><li>first<i class="fas fa-chevron-right"></i></li></a>
+                        <a href="albums.html?name=${item.name}"><li>second<i class="fas fa-chevron-right"></i></li></a>
+                        <a href="albums.html?name=${item.name}"><li>third<i class="fas fa-chevron-right"></i></li></a>
                     </ul>
                 </div>`;
         })
     }); 
-
-    /*
-    */
--- src/js/fetch-index.js
+++ src/js/fetch-index.js
@@ -1,24 +1,23 @@
-//Letter problems
-
-fetch("https://api.spotify.com/v1/browse/new-releases", {
-    method: "GET",
+fetch("https://api.spotify.com/v1/browse/featured-playlists", { 
     headers: {
-        Authorization: `Bearer BQAI3NIQ2Scz7bfQleKpzSf_4sGh1O6fQvxDS-86YT9k78DW2b0Pe6dshzA4roL8YzisPJy16Z23sX9CqJItu12bLGnvuiOslnUrp5RxREvIQa9yofeUT--DIFlHg0xR4x9Gi5TnBoCu2Zp2qDOnIw`     
+        "Accept" : "application/json",
+        "Content-Type" : "application/x-www-form-urlencoded",
+        "Authorization": "Bearer BQABT3PQ5zqqUASD9yCBVj_cjRxj4kDI1vfu8jyvneoDmt3gPbfTJNIz0OxRB6Y7axUmx-OcUHVwLIVh2Jw"
     }
     })
-    
-    .then(response => response.json())
-    .then(function(result) {
-        console.log(result.albums.items);     
-        result.albums.items.forEach(item =>{
-            document.querySelector(".featured").innerHTML +=`
-                <div class="featured_album">
-                    <div class="featured_text">
-                        <h2>${item.name}</h2>
-                        <p>${item.type}</p>
+        .then(response => response.json())
+        .then(function(result) {
+            result.playlists.items.forEach(item =>{
+                document.querySelector(".featured").innerHTML +=`
+                    <div class="featured_album">
+                        <div class="featured_text">
+                            <h2>${item.name}</h2>
+                            <p>${item.type}</p>
+                        </div>
+                        <a href="player.html?id=${item.id}"><img src="${item.images[0].url}" alt="${item.name}"></a>
                     </div>
-                    <a href="albums.html?id=${item.id}"><img src="${item.images[0].url}" alt="${item.name}"></a>
-                </div>
-            `;
+                `;
         })
     }); 
+
+      
